5e82b2834fc8bd19b93881ddb73b0149536fd9e4
Elias Barroso Jr <eliasbarroso222@gmail.com>
2025-01-30 15:58:24 -0300

    feat: new project.

--- src/utils/userData.ts
+++
@@ -1,9 +0,0 @@
-export const userData = {
-  nameUser: "Elias Barroso",
-  githubUser: "Eliasbarrosojr",
-  linkedinUser: "elias-barroso-785436251",
-  whatsappNumber: "+5598984359726",
-  emailUser: "eliasbarroso222@gmail.com",
-  instagramUser: "elias.b_jr",
-  facebookUser: "",
-};
